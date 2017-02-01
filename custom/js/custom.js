45621d3d37bf8bfada9beb3303014fecf778ba37
artemilchuk <artem.ilchuk@ed-era.com>
2017-02-01 05:19:10 +0000

    Updates custom/js/custom.js
    Auto commit by GitBook Editor

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -3,11 +3,6 @@ $(document).ready(function() {
     var href, $lastLink;
 
     $("a.toggle-search").remove();
-
-    $lastLink = $(".custom-link:last");
-    href = $lastLink.prop("href");
-    href = "https://ed-era.com/books/" + _.last(href.split("-"));
-    $lastLink.prop("href", href);
   }
   init();
   require(["gitbook"], function (gitbook) {
