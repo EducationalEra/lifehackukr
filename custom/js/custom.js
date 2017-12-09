6743a5fb468fa82708bd24f698aeac42c10f738c
artemilchuk <artem.ilchuk@ed-era.com>
2017-12-09 15:02:11 +0200

    fix mathax plugin

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -6,8 +6,25 @@ $(document).ready(function() {
     $lastLink = $(".custom-link:last");
     $lastLink.remove();
   }
+  
   init();
   require(["gitbook"], function (gitbook) {
+    function startMathJax() {
+      MathJax.Hub.Config({
+        tex2jax: {}
+      });
+
+      gitbook.events.bind("page.change", function () {
+        MathJax.Hub.Typeset();
+      });
+    }
     gitbook.events.bind("page.change", init);
+    var interval = null
+    interval = setInterval(function () {
+      if (typeof MathJax === "object") {
+        startMathJax()
+        clearInterval(interval);
+      }
+    }, 1000);
   });
 });
